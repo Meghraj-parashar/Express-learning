3cb7920f18aaedadbe8ed31e604d4755890eba24
Meghraj-parashar <meghrajparashar@gmail.com>
2025-03-09 15:37:40 +0530

    multiple parametre and query

--- express-practice-set/1-practice.js
+++ express-practice-set/1-practice.js
@@ -14,8 +14,26 @@ app.get('/', (req, res) => {
 //get request at /greet path for then send a greeting message
 app.get('/greet', (req, res) => {
     const name = req.query.name;
-    res.send(`<h1>HELLO MY BOY ${name}</h1>`);
+    res.send(`<h1>HELLO MY BOY ${name.toUpperCase()}</h1>`);
 });
+
+//               second practice set
+//         MULTIPLE PARAMETER & QUERY STRINGS
+
+app.get('/post/:postid/commemts', (req, res) => {
+    const postid = req.params.postid;
+    const query = req.query;
+    const { name } = req.query;
+    
+    console.log(query, postid);  
+    res.send(`<h1>Post ${postid} and query is ${query} and name is ${name.toUpperCase()}  </h1>`);
+});
+
+
+
+
+
+
 //listen to port
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
